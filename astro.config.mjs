5754e5b56ebc3ac37a2056d43d170910fcb31f51
Lukas Chylik <chylik.lukas@gmail.com>
2025-08-25 21:11:01 +0200

    feat: add syntax highlighting with Shiki and implement scroll-state queries presentation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,22 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    port: 4444,
+  },
+  integrations: [
+    mdx({
+      syntaxHighlight: 'shiki',
+      shikiConfig: {
+        theme: 'github-dark',
+        wrap: true,
+      },
+    })
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
